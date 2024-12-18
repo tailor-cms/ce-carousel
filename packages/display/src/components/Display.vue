2f9a5719f4e8d5943fd908e8c9da5edc5d36f000
rkusan00 <renato.kusanovic@gmail.com>
2024-12-18 10:17:35 +0100

    Implement display component

--- packages/display/src/components/Display.vue
+++ packages/display/src/components/Display.vue
@@ -1,30 +1,47 @@
 <template>
-  <div class="tce-root">
-    <p>This is the Display version of the content element id: {{ id }}</p>
-    <v-btn class="my-6" @click="submit">Update user state</v-btn>
-    <div>
-      <div class="mb-1 text-subtitle-2">User state:</div>
-      <pre class="text-body-2">{{ userState }}</pre>
-    </div>
-  </div>
+  <VCard class="tce-root">
+    <VCarousel :height="data.height" :show-arrows="false">
+      <VCarouselItem v-for="item in data.items" :key="item.id">
+        <div class="pa-4">
+          <VAlert v-if="!embeds[item.id].length" type="info" variant="tonal">
+            No content elements added to this item.
+          </VAlert>
+          <TailorEmbeddedContainer v-else :elements="embeds[item.id]" />
+        </div>
+      </VCarouselItem>
+    </VCarousel>
+  </VCard>
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue';
 import { ElementData } from '@tailor-cms/ce-carousel-manifest';
+import sortBy from 'lodash/sortBy';
 
-const props = defineProps<{ id: number; data: ElementData; userState: any }>();
-const emit = defineEmits(['interaction']);
+const props = defineProps<{ data: ElementData; userState: any }>();
+defineEmits(['interaction']);
 
-const submit = () => emit('interaction', { id: props.id });
+const embeds = computed(() => {
+  const { items, embeds } = props.data;
+  return items.reduce(
+    (acc, item) => {
+      const itemEmbeds = item.elementIds.map((id) => embeds[id]);
+      acc[item.id] = sortBy(itemEmbeds, 'position');
+      return acc;
+    },
+    {} as Record<string, any[]>,
+  );
+});
 </script>
 
 <style scoped>
 .tce-root {
-  background-color: transparent;
-  margin-top: 1rem;
-  padding: 1.25rem;
-  border: 2px dashed #888;
   font-family: Arial, Helvetica, sans-serif;
   font-size: 1rem;
+}
+
+.v-carousel-item :deep(.v-responsive__content) {
+  padding-bottom: 2rem;
+  overflow-y: auto;
 }
 </style>
